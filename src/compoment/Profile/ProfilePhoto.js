f014c97c6c1f1ea22343836eb8a4042ad6e5d2b3
ben yedder <benyedderkoubaib@gmail.com>
2022-05-03 14:14:13 +0100

    create a ProfilePhoto.js compoment

---
+++ src/compoment/Profile/ProfilePhoto.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import image from "../../image/image.jpg"
+const ProfilePhoto = () => {
+  return (
+    <div><img src={image} /></div>
+  )
+}
+
+export default ProfilePhoto
